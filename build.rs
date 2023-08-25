96c7422583084746fbdc3ef7ccb51addfe2c46a0
xd009642 <danielmckenna93@gmail.com>
2023-08-25 08:55:42 +0100

    Fix build.rs

--- build.rs
+++ build.rs
@@ -1,10 +1,6 @@
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     tonic_build::configure()
-        .build_server(true)
-        .server_mod_attribute("attrs", "#[tonic_mock::mock]")
-        .build_client(true)
-        .client_mod_attribute("attrs", "#[cfg(feature = \"client\")]")
-        .out_dir(".")
+        .server_mod_attribute("routeguide", "#[tonic_mock::mock]")
         .compile(
             &["tests/protos/routeguide/route_guide.proto"],
             &["tests/protos"],
